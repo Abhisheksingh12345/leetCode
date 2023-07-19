43caa1bf47a85d215ab7f472d787bfee5e86e4d9
Abhisheksingh12345 <atching123@gmail.com>
2023-07-20 02:52:19 +0530

    plus one

--- array/problem_66/plus_one.cpp
+++ array/problem_66/plus_one.cpp
@@ -5,18 +5,28 @@ using namespace std;
 class Solution
 {
 public:
-    int plusOne(vector<int> &nums)
+    vector<int> plusOne(vector<int> &digits)
     {
+        if((digits[digits.size()-1] + 1) > 9) {
+            digits.pop_back();
+            digits.push_back(1);
+            digits.push_back(0);
+        } else {
+            digits[digits.size()-1] = digits[digits.size()-1] + 1;
+        }
+        return digits;
 
-        
     }
 };
 
     int main()
     {
         Solution s;
-        int myints[] = {1,2,3};
+        int myints[] = {9};
         std::vector<int> nums(myints, myints + sizeof(myints) / sizeof(int));
-        cout << s.plusOne(nums);
+        vector<int> digits = s.plusOne(nums);
+        for(int i : digits) {
+            cout << i << " ";
+        }
         return 0;
     }
