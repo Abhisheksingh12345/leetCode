d737cde9394be50af52b8e64b1996182f8cb8919
Abhisheksingh12345 <atching123@gmail.com>
2023-07-20 02:41:04 +0530

    intial commit

---
+++ array/problem_66/plus_one.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int plusOne(vector<int> &nums)
+    {
+
+        
+    }
+};
+
+    int main()
+    {
+        Solution s;
+        int myints[] = {1,2,3};
+        std::vector<int> nums(myints, myints + sizeof(myints) / sizeof(int));
+        cout << s.plusOne(nums);
+        return 0;
+    }
