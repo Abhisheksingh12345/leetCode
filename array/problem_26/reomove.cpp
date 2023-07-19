3aaab584943dfef9fa4eaeae8acfb69c7a480108
Abhisheksingh12345 <atching123@gmail.com>
2023-07-20 01:32:20 +0530

    intial commit

--- array/problem_26/reomove.cpp
+++ array/problem_26/reomove.cpp
@@ -1,16 +1,21 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void removeDuplicates(vector<int>& nums) {
-
+    int removeDuplicates(vector<int> &nums)
+    {
     }
 };
 
+int main()
+{
+    Solution s;
+    int myints[] = {1, 2, 2, 3};
+    std::vector<int> nums(myints, myints + sizeof(myints) / sizeof(int));
+    cout << s.removeDuplicates(nums);
 
-int main() {
-    cout << "Hello World";
     return 0;
 }
