f9582507ca8353b5a700a251e50af2a0534b3f82
Abhisheksingh12345 <atching123@gmail.com>
2023-07-20 01:52:16 +0530

    used a new array t->O(n)

--- array/problem_26/reomove.cpp
+++ array/problem_26/reomove.cpp
@@ -7,6 +7,14 @@ class Solution
 public:
     int removeDuplicates(vector<int> &nums)
     {
+        vector<int> new_nums;
+        new_nums.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (new_nums.back() != nums[i])
+                new_nums.push_back(nums[i]);
+        }
+        return new_nums.size();
     }
 };
 
@@ -16,6 +24,5 @@ int main()
     int myints[] = {1, 2, 2, 3};
     std::vector<int> nums(myints, myints + sizeof(myints) / sizeof(int));
     cout << s.removeDuplicates(nums);
-
     return 0;
 }
