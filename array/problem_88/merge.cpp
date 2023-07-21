1f947f46802776255d3aa6f4c3b7f70d7c0c4e11
Mokey-D-luffy88 <captaine0luffy@gmail.com>
2023-07-22 01:42:27 +0530

    initial commit

---
+++ array/problem_88/merge.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+    }
+};
+int main()
+{
+    int arr[]{1, 2, 3, 0, 0, 0};
+    int m = 3;
+    int arr2[] { 2, 5, 6 };
+    int n = 3;
+    Solution s;
+
+
+}
