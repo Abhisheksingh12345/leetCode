7b4c146fa9c48ce218e62d985e4b1cb9b808615d
Mokey-D-luffy88 <captaine0luffy@gmail.com>
2023-07-22 01:50:58 +0530

    num1, num2

--- array/problem_88/merge.cpp
+++ array/problem_88/merge.cpp
@@ -7,15 +7,20 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
+        
+        
     }
 };
 int main()
 {
+    Solution s;
     int arr[]{1, 2, 3, 0, 0, 0};
     int m = 3;
     int arr2[] { 2, 5, 6 };
     int n = 3;
     Solution s;
-
+    vector<int> nums1(arr, arr + sizeof(arr) / sizeof(int));
+    vector<int> nums2(arr2, arr2 + sizeof(arr2) / sizeof(int));
+    s.merge(nums1, m, nums2, n);
 
 }
