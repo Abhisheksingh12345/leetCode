d7dea7e36ea60a275ab78c24074eff666303f35a
Abhishek <atching123@gmail.com>
2023-09-15 00:14:47 +0530

    using xor property

--- array/problem_136/single_no.cpp
+++ array/problem_136/single_no.cpp
@@ -6,6 +6,10 @@ using namespace std;
 class Solution {
     public :
         int singleNumber(vector<int> nums) {
+            int res = 0;
+            for(int i : nums) 
+                res ^= i;
+            return res;
         }
 };
 
