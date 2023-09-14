cc1b7e4e12679b92a8eb63024a0cdbc695fcfb09
Abhishek <atching123@gmail.com>
2023-09-15 01:05:55 +0530

    sort then check

--- array/problem_217/contains_duplicate.cpp
+++ array/problem_217/contains_duplicate.cpp
@@ -3,18 +3,27 @@
 
 using namespace std;
 
-class Solution {
-    public :
-        int containsDuplicate(vector<int> nums) {      
+class Solution
+{
+public:
+    int containsDuplicate(vector<int> nums)
+    {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+                return true;
         }
+        return false;
+    }
 };
 
-int main() {
-    int arr[] {2,2,1,1,1,2,2};
+int main()
+{
+    int arr[]{2, 2, 1, 1, 1, 2, 2};
     vector<int> nums;
-    for(int i : arr) 
+    for (int i : arr)
         nums.push_back(i);
     Solution s;
     cout << s.containsDuplicate(nums);
-    
 }
