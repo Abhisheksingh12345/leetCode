1cdbd9d6c29e993117e1d6a8a1dc01e63681d4fc
Abhishek <atching123@gmail.com>
2023-09-15 00:13:52 +0530

    array to vector conversion

--- array/problem_136/single_no.cpp
+++ array/problem_136/single_no.cpp
@@ -10,5 +10,12 @@ class Solution {
 };
 
 int main() {
+    int arr[] {4,1,2,1,2};
+    vector<int> nums;
+    for(int i  :arr) {
+        nums.push_back(i);
+    }
+    Solution s;
+    cout << s.singleNumber(nums);
     return 0;
 }
