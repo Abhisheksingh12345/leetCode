13ad4b189644d72d4467905089ae9e5bf9faec5f
Abhishek <atching123@gmail.com>
2023-09-27 22:53:15 +0530

    including iostream lib

--- array/problem_118/pascal.cpp
+++ array/problem_118/pascal.cpp
@@ -2,8 +2,21 @@
 #include <vector>
 using namespace std;
 vector<vector<int>> pascal_triangle(int numrow);
-int main() {
+int main()
+{
     int numrow = 5;
-    vector<vector<int>> pt= pascal_triangle(numrow); 
+    vector<vector<int>> pt = pascal_triangle(numrow);
     return 0;
+}
+vector<vector<int>> pascal_triangle(int numrow)
+{
+    vector<vector<int>> outter;
+    vector<int> inner{1};
+    outter.push_back(inner);
+    if (numrow == 1)
+        return outter;
+    for (int i = 1; i < numrow; i++)
+    {
+    }
+    return outter;
 }
